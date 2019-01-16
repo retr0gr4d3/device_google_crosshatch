e29d37d15a0c1db13b383714219fa8d3c8172ee4
stebomurkn420 <stevn4127@gmail.com>
2019-01-16 13:52:23 -0500

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -26,7 +26,7 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
 # Include DU common configuration
-include vendor/bootleggers/config/common_full_phone.mk
+include vendor/liquid/config/common_full_phone.mk
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
@@ -35,7 +35,7 @@ $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-PRODUCT_NAME := bootleg_crosshatch
+PRODUCT_NAME := liquid_crosshatch
 PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 3 XL
