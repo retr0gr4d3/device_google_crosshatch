91254eaeaafd063b8ac5106ee00bb6bcd88158df
Jake Hurd <legendbreakerjake@gmail.com>
2020-03-19 23:18:20 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -67,10 +67,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-ifeq ($(wildcard vendor/google_devices/crosshatch/proprietary/device-vendor-crosshatch.mk),)
-    BUILD_WITHOUT_VENDOR := true
-endif
-
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/google/crosshatch-kernel/Image.lz4
 else
